2a5934cff2bb8d2c61eb676676c07ab24803859f
mahesware <umamaheswari539@gmail.com>
2025-08-21 13:40:35 +0530

    Update app.js- Basic Form with event listeneres

--- app.js
+++ app.js
@@ -34,14 +34,12 @@ const html = `
     <title>Hello from Render!</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
+     document.getElementById("push").addEventListener("click",async(e) =>{
+     alert('user has tried to push the data..');
+     })
+     document.getElementById("pop").addEventListener("click",async(e) =>{
+     alert('user has tried to pop the data..');
+     })
     </script>
     <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
@@ -72,7 +70,12 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+    <form id="stackAppForm">
+    <input type="text" id="size"  placeholder="Enter Stack Size" required/> 
+    <button type="button" id="push" >Push</button>
+    <button type="button" id="pop" > Pop</button>
+    <div id="stack"></div>
+    </form>
     </section>
   </body>
 </html>
